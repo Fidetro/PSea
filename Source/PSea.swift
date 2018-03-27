fc23377d3c85f220a2fe4fe5e40a7b7f452b0079
fidetro <zykzzzz@hotmail.com>
2018-03-27 13:33:45 +0800

    use generics

--- Source/PSea.swift
+++ Source/PSea.swift
@@ -8,15 +8,15 @@
 
 import UIKit
 import Alamofire
-
-public typealias SccuessCallBack = ((_ response:DataResponse<Any>,_ data:Any)->())
+public typealias SccuessCallBack = ((_ parseValue:Any?,_ data:Any)->())
 public typealias ErrorCallBack = ((_ response:DataResponse<Any>,_ code:Int,_ message:String)->())
-public typealias FailureCallBack = ((_ response:DataResponse<Any>,_ request:URLRequest?,_ error:Error?)->())
+public typealias FailureCallBack = ((_ response:DataResponse<Any>,_ error:Error)->())
 
 public protocol PSea: class {
     var successHandler : SccuessCallBack?{get set}
     var errorHandler : ErrorCallBack?{get set}
     var failureHandler : FailureCallBack?{get set}
+    
     /// 请求方式
     func method() -> HTTPMethod
     /// 设置域名
@@ -31,9 +31,9 @@ public protocol PSea: class {
     func encoding() -> ParameterEncoding
     
     func complete(_ completionHandler: @escaping ((DataResponse<Any>) -> ()))
-    func successParse(response:DataResponse<Any>)
-    func errorParse(response:DataResponse<Any>)
-    func failureParse(response:DataResponse<Any>)
+    func successParse(response: DataResponse<Any>)
+    func errorParse(response: DataResponse<Any>)
+    func failureParse(response:DataResponse<Any>,error: Error)
     
 }
 
@@ -43,27 +43,57 @@ extension PSea {
         let url = baseURL()+requestURI()
         Alamofire.request(url, method: method(), parameters: parameters(), encoding: encoding(), headers: headers()).responseJSON(completionHandler: completionHandler)
     }
-    @discardableResult public func success(_ success:SccuessCallBack?) -> PSea {
-        
-        self.successHandler = success
+    
+    @discardableResult
+    public func success<T:Decodable>(_ type:T.Type?=nil,_ success:((_ response:T?,_ data:Any)->())?) -> PSea {
+        self.successHandler = { (parse,data) in
+            do{
+                if let handler = success {
+                    let data = try JSONSerialization.data(withJSONObject: parse as Any, options: .prettyPrinted)
+                    let model = try JSONDecoder().decode(T.self, from: data)
+                    handler(model,data)
+                }
+            }catch{
+                if let handler = success {
+                    handler(nil,data)
+                }
+            }
+        }
         return self
     }
     
-    @discardableResult public func error(_ error:ErrorCallBack?) -> PSea {
+    @discardableResult
+    public func success(_ success:((_ response:Any?,_ data:Any)->())?) -> PSea {
+        self.successHandler = { (parse,data) in
+                if let handler = success {
+                    handler(parse,data)
+                }
+        }
+        return self
+    }
+    
+    @discardableResult
+    public func error(_ error:ErrorCallBack?) -> PSea {
         self.errorHandler = error
         return self
     }
     
-    @discardableResult public func failure(_ failure:FailureCallBack?) -> PSea {
-        
+    @discardableResult
+    public func failure(_ failure:FailureCallBack?) -> PSea {
         self.failureHandler = failure
         return self
     }
-    @discardableResult public func request() -> PSea {
+    
+    @discardableResult
+    public func request() -> PSea {
         complete {  (response) in
-            self.successParse(response: response)
-            self.errorParse(response: response)
-            self.failureParse(response: response)
+            switch response.result {
+            case .success( _):
+                self.successParse(response: response)
+                self.errorParse(response: response)
+            case .failure(let error):
+                self.failureParse(response: response, error: error)
+            }
         }
         return self
     }
