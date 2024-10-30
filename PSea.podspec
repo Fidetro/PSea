23ea358e2feabeff2ac578dc334f783c918c7b1c
Fidetro <zykzzzz@hotmail.com>
2024-10-31 00:28:04 +0800

    适配af5.10.1

--- PSea.podspec
+++ PSea.podspec
@@ -1,14 +1,14 @@
 Pod::Spec.new do |s|
 
   s.name         = "PSea"
-  s.version      = "1.2.1"
+  s.version      = "1.2.2"
   s.summary      = "PSea"
   s.homepage     = "https://github.com/Fidetro/PSea"
   s.license      = { :type => "MIT", :file => "LICENSE" }
   s.author       = { "fidetro" => "zykzzzz@hotmail.com" }
-  s.platform     = :ios, "8.0"
+  s.platform     = :ios, "10.0"
   s.source       = { :git => "https://github.com/Fidetro/PSea.git", :tag => "#{s.version}" }
   s.source_files  = "Source/PSea/*.{h,m,swift}"
-  s.dependency "Alamofire","~> 4.7.3"
+  s.dependency "Alamofire","~> 5.10.1"
 
 end
--- Source/PSea/PSea.swift
+++ Source/PSea/PSea.swift
@@ -8,20 +8,41 @@
 
 import UIKit
 import Alamofire
-public typealias SccuessCallBack = ((_ parseValue:Any?,_ data:Any)->())
-public typealias ErrorCallBack = ((_ response:DataResponse<Any>,_ code:Int,_ message:String)->())
-public typealias FailureCallBack = ((_ response:DataResponse<Any>,_ error:Error)->())
-public typealias ProgressCallBack = ((_ progress:Progress)->())
+public typealias ProgressHandler = @Sendable (_ progress: Progress) -> Void
+public protocol PSeaType: AnyObject {
 
-open class PSea: PSeaType {
-    public var successHandler: SccuessCallBack?
     
-    public var errorHandler: ErrorCallBack?
+    init()
+    /// 请求方式
+    func method() -> HTTPMethod
+    /// 设置域名
+    func baseURL() -> String
+    /// 请求路由
+    func requestURI() -> String
+    /// 请求参数
+    func parameters() -> Parameters?
+    /// 请求头
+    func headers() -> HTTPHeaders?
+    /// 参数编码
+    func encoding() -> ParameterEncoding
+
+    func requestInterval() -> TimeInterval
+}
+
+extension PSeaType {
     
-    public var failureHandler: FailureCallBack?
+    public func requestInterval() -> TimeInterval {
+        return 0.0
+    }
     
-    public var progressHandler: ProgressCallBack?
-    
+}
+
+public enum PSeaError: Error {
+    case failure(_ error: Error)
+}
+
+open class PSea : PSeaType {
+
     required public init() { }
     
     open func method() -> HTTPMethod {
@@ -52,161 +73,52 @@ open class PSea: PSeaType {
         return URLEncoding(destination: .httpBody)
     }
     
-    open func successParse(response: DataResponse<Any>) {
-        
-    }
     
-    open func errorParse(response: DataResponse<Any>) {
-        
-    }
-    
-    open func failureParse(response: DataResponse<Any>, error: Error) {
-        
-    }
-    
-    open func progressParse(progress: Progress) {
-        if let progressHandler = progressHandler {
-            progressHandler(progress)
-        }
-    }
-    
-    @discardableResult
-    open func request() -> PSea {
-        
+    public func request<T: Decodable>(_ t: T.Type,completionHandler: @escaping (Result<T, PSeaError>) -> Void) -> PSea {
         guard PSeaQueue.share.set(object: self) else { return self }
-        
-        request { [weak self] (response) in
+
+        let url = baseURL()+requestURI()
+        AF.request(url, method: method(), parameters: parameters(), encoding: encoding(), headers: headers()).responseData { response in
             switch response.result {
-            case .success( _):
-                self?.successParse(response: response)
-                self?.errorParse(response: response)
-            case .failure(let error):
-                self?.failureParse(response: response, error: error)
+            case .success(let data) :
+                do {
+                    let model = try JSONDecoder().decode(t, from: data)
+                    completionHandler(.success(model))
+                } catch {
+                    completionHandler(.failure(.failure(error)))
+                }
+            case .failure(let error) :
+                completionHandler(.failure(.failure(error)))
             }
         }
         return self
     }
     
-    open func upload(multipartFormData: @escaping (MultipartFormData) -> Void) -> PSea {
+    open func upload<T: Decodable>(_ t: T.Type,multipartFormData: @escaping (MultipartFormData) -> Void, progressHandler: ProgressHandler?, completionHandler: @escaping (Result<T, PSeaError>) -> Void) -> PSea {
         
         guard PSeaQueue.share.set(object: self) else { return self }
-        
         let url = baseURL()+requestURI()
-        Alamofire.upload(multipartFormData: multipartFormData, to: url, method: method(), headers: headers()) { [weak self] (encodingResult) in
-            switch encodingResult {
-            case .success(let request, _, _):
-                request.uploadProgress { (progress) in
-                    self?.progressParse(progress: progress)
-                }
-                request.responseJSON(completionHandler: { (response) in
-                    switch response.result {
-                    case .success(_):
-                        self?.successParse(response: response)
-                        self?.errorParse(response: response)
-                    case .failure(let error):
-                        self?.failureParse(response: response, error: error)
-                    }
-                })
-                
-            case .failure(let error):
-                self?.failureParse(response: DataResponse(request: nil, response: nil, data: nil, result: .failure(error)), error: error)
-            }
-        }
-        return self
-    }
-    
-    @discardableResult
-    open func success<T:Decodable>(_ type:T.Type?=nil,_ success:((_ response:T?,_ data:Any)->())?) -> PSea {
-        self.successHandler = { (parse,data) in
-            do{
-                if let handler = success {
-                    if let parseData = parse as? [String:Any] {
-                        let data = try JSONSerialization.data(withJSONObject: parseData as Any, options: .prettyPrinted)
-                        let model = try JSONDecoder().decode(T.self, from: data)
-                        handler(model,data)
-                    }else if let parseData = parse as? [Any] {
-                        let data = try JSONSerialization.data(withJSONObject: parseData as Any, options: .prettyPrinted)
-                        let model = try JSONDecoder().decode(T.self, from: data)
-                        handler(model,data)
-                    }else if let parseData = parse as? T {
-                        handler(parseData,data)
-                    }else{
-                        handler(nil,data)
+        AF.upload(multipartFormData: multipartFormData, to: url, method: method(), headers: headers()) .uploadProgress { progress in
+            progressHandler?(progress)
+        }.response { response in
+            switch response.result {
+            case .success(let data) :
+                if let data = data {
+                    do {
+                        let model = try JSONDecoder().decode(t, from: data)
+                        completionHandler(.success(model))
+                    } catch {
+                        completionHandler(.failure(.failure(error)))
                     }
                 }
-            }catch{
-                if let handler = success {
-                    handler(nil,data)
-                }
+            case .failure(let error) :
+                completionHandler(.failure(.failure(error)))
             }
         }
         return self
     }
-    
-    @discardableResult
-    public func success(_ success:((_ response:Any?,_ data:Any)->())?) -> PSea {
-        self.successHandler = { (parse,data) in
-            if let handler = success {
-                handler(parse,data)
-            }
-        }
-        return self
-    }
-    
-    @discardableResult
-    public func error(_ error:ErrorCallBack?) -> PSea {
-        self.errorHandler = error
-        return self
-    }
-    
-    @discardableResult
-    public func failure(_ failure:FailureCallBack?) -> PSea {
-        self.failureHandler = failure
-        return self
-    }
-    @discardableResult
-    public func progress(_ progressHandler:ProgressCallBack?) -> PSea {
-        self.progressHandler = progressHandler
-        return self
-    }
-}
 
-public protocol PSeaType: AnyObject {
-    var successHandler : SccuessCallBack?{get set}
-    var errorHandler : ErrorCallBack?{get set}
-    var failureHandler : FailureCallBack?{get set}
     
-    init()
-    /// 请求方式
-    func method() -> HTTPMethod
-    /// 设置域名
-    func baseURL() -> String
-    /// 请求路由
-    func requestURI() -> String
-    /// 请求参数
-    func parameters() -> Parameters?
-    /// 请求头
-    func headers() -> HTTPHeaders?
-    /// 参数编码
-    func encoding() -> ParameterEncoding
-    
-    func request(_ completionHandler: @escaping ((DataResponse<Any>) -> ()))
-    func successParse(response: DataResponse<Any>)
-    func errorParse(response: DataResponse<Any>)
-    func failureParse(response:DataResponse<Any>,error: Error)
-    func requestInterval() -> TimeInterval
-}
 
-extension PSeaType {
-    
-    public func requestInterval() -> TimeInterval {
-        return 0.0
-    }
-    
-    public func request(_ completionHandler: @escaping ((DataResponse<Any>) -> ())) {
-        let url = baseURL()+requestURI()
-        Alamofire.request(url, method: method(), parameters: parameters(), encoding: encoding(), headers: headers()).responseJSON(completionHandler: completionHandler)
-    }
-    
+       
 }
-
